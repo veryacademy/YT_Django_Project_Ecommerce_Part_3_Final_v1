fc19a183e92f13f5482fb03bbc20dd6c4179633a
veryacademy <35196453+veryacademy@users.noreply.github.com>
2021-02-17 18:31:08 +0000

    Update forms.py

--- account/forms.py
+++ account/forms.py
@@ -21,7 +21,7 @@ class UserLoginForm(AuthenticationForm):
 class RegistrationForm(forms.ModelForm):
 
     user_name = forms.CharField(
-        label='Enter Username', min_length=4, max_length=50, help_text='Required')
+        label='Enter Email', min_length=4, max_length=50, help_text='Required')
     email = forms.EmailField(max_length=100, help_text='Required', error_messages={
         'required': 'Sorry, you will need an email'})
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
